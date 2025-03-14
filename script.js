8bd1ef727d968068da23b245cac7307040a338a4
Victor-JG <vjuagar42@gmail.com>
2025-03-14 08:25:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-onst canvas = document.getElementById("gameCanvas");
+const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
